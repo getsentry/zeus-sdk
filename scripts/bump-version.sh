7d4cddd642827522bb593398a533a8b2b45f585b
Anton Ovchinnikov <anton@tonyo.info>
2018-06-30 20:00:48 +0200

    feat: Use "craft" for releases

---
+++ scripts/bump-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eux
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR/..
+
+if [ -z "${1:-}" ]; then
+    set -- "patch"
+fi
+
+NPM_VERSION=$(npm version $1)
+VERSION=${NPM_VERSION:1}
+
+git commit -am "release: $VERSION"
