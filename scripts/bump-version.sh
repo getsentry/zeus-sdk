9941817aa7fdf63d8a1f4b9e1745920265dd43c7
Anton Ovchinnikov <anton@tonyo.info>
2018-09-24 18:22:44 +0200

    ref: Update version bumping script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,12 +4,9 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 
-if [ -z "${1:-}" ]; then
-    set -- "patch"
-fi
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
 
+# Do not tag and commit changes made by "npm version"
 export npm_config_git_tag_version=false
-NPM_VERSION=$(npm version $1)
-VERSION=${NPM_VERSION:1}
-
-git commit -am "release: $VERSION"
+npm version "${NEW_VERSION}"
